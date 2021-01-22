a6950f306f969f9c5c0c58b9a7e4bef4f853b641
MamontovDN <mamontovdn@yandex.ru>
2021-01-22 13:45:10 +0300

    added placeholders

--- users/forms.py
+++ users/forms.py
@@ -6,10 +6,23 @@ from .models import User
 
 
 class RegForm(RegistrationForm):
-    first_name = forms.CharField(required=True, max_length=150, label="Имя")
+    first_name = forms.CharField(
+        required=True, max_length=150, label="Имя",
+        widget=forms.TextInput(attrs={'placeholder': 'Иван'}))
     last_name = forms.CharField(required=True, max_length=150, label="Фамилия")
 
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username', 'email',
                   'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs.update({'placeholder': 'Иван'})
+        self.fields['last_name'].widget.attrs.update({'placeholder': 'Иванов'})
+        self.fields['username'].widget.attrs.update({'placeholder': 'login'})
+        self.fields['email'].widget.attrs.update(
+            {'placeholder': 'ivan@yandex.ru'})
+        self.fields['password1'].widget.attrs.update({'placeholder': 'пароль'})
+        self.fields['password2'].widget.attrs.update(
+            {'placeholder': 'повторите пароль'})
