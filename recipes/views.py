e0628ff4e2686889775521a823634007a26609df
mamontovdn <mamontovdn@yandex.ru>
2021-01-23 23:39:34 +0300

    reset password

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    if request.user.is_authenticated:
-        return render(request, 'index.html')
-    else:
-        return render(request, 'index.html')
+    return render(request, 'index.html')
