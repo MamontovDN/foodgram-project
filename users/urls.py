5919f44214579f58611e400fa20c9e4030518694
mamontovdn <mamontovdn@yandex.ru>
2021-02-15 19:41:12 +0300

    fix users templates

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('password/reset/',
+         auth_views.PasswordResetView.as_view(
+             success_url=reverse_lazy('auth_password_reset_done'),
+             html_email_template_name='registration/password_reset_email.html'),
+         name='auth_password_reset'),
     path('', include('registration.backends.default.urls')),
 ]
